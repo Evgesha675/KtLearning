b45205c57a69bf02ce189528dff3a5ef236ffd5d
Egesha675 <tetenkinevgenij@gmail.com>
2024-12-03 19:41:48 +0800

    snc

--- fib.kt
+++ fib.kt
@@ -1,7 +1,31 @@
-fun main() {
-    val числа = listOf(1, 2, 3, 4, 5)
-    val сумма = числа.sum()
+import java.io.PrintStream
 
-    println("Сумма чисел: $сумма")
-    println("Тест кириллицы: Привет мир!")
+fun main() {
+    System.setOut(PrintStream(System.out, true, "UTF-8"))
+
+    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+
+    fun fib(passedMonth: Int, maxAge: Int, matureMonth: Int = 2): LongArray {
+        val currentGeneration = LongArray(maxAge) { 0L }
+        currentGeneration[0] = 1L 
+
+        for (i in 1 until passedMonth) {
+            val newGeneration = LongArray(maxAge) { 0L }
+            newGeneration[0] = currentGeneration.slice(matureMonth - 1 until maxAge).sum()
+
+            for (j in 1 until maxAge) {
+                newGeneration[j] = currentGeneration[j - 1]
+            }
+
+            for (i in currentGeneration.indices) {
+                currentGeneration[i] = newGeneration[i]
+            }
+        }
+
+        return currentGeneration
+    }
+
+    println("[${fib(n, m).joinToString(", ")}]")
+
+    println(fib(n, m).sum())  
 }
